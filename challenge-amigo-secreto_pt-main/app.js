f6cd98129018203799ea39f4424d5ceb45548d66
Lais Lopes <laisllopess21@gmail.com>
2025-03-06 17:05:15 -0300

    iniciando projeto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,10 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Tarefa 1 - Implementando função para adicionar amigos
+  // Tarefa 1.1 - Capturar o valor do campo de entrada
+
+let nomeAmigos = [];
+
+function adicionarAmigo(){
+
+}
