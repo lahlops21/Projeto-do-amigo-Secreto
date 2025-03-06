980168b5cf3095152ceb892e4cd3fbaea35ae8d7
Lais Lopes <laisllopess21@gmail.com>
2025-03-06 18:05:15 -0300

    Colocando os valores dentro de listas e limpando o campo de entrada

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -3,16 +3,22 @@
 // Tarefa 1 - Implementando função para adicionar amigos
 // Tarefa 1.1 - Capturar o valor do campo de entrada
 
-let nomeAmigos = [];
+let listaAmigos = [];
 
 function adicionarAmigo() {
   // Capturando o valor do id 'amigo' e o guardando em uma variável
   let nomeAmigo = document.getElementById("amigo").value.trim();
-  
+
   // Tarefa 1.2 - Validando a entrada
-  if (nomeAmigo == '') {
+  if (nomeAmigo == "") {
     alert("Por favor insira um nome!");
-    return
-  } 
-  console.log(nomeAmigo);
+    return;
+  } else {
+    // Tarefa 1.3 - Capturando nomes válidos e os colocando no Array
+    listaAmigos.push(nomeAmigo);
+  }
+  // Tarefa 1.4 - Limpando o campo de entrada
+  document.getElementById("amigo").value = "";
+
+  console.log(listaAmigos);
 }
