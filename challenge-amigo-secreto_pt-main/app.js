e7abd0e51264d0192cec6d1227ee2e49d63c956a
Lais Lopes <laisllopess21@gmail.com>
2025-03-06 17:31:39 -0300

    Capturando valores do input e validando entrada

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,10 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 // Tarefa 1 - Implementando função para adicionar amigos
-  // Tarefa 1.1 - Capturar o valor do campo de entrada
+// Tarefa 1.1 - Capturar o valor do campo de entrada
 
 let nomeAmigos = [];
 
-function adicionarAmigo(){
-
+function adicionarAmigo() {
+  // Capturando o valor do id 'amigo' e o guardando em uma variável
+  let nomeAmigo = document.getElementById("amigo").value.trim();
+  
+  // Tarefa 1.2 - Validando a entrada
+  if (nomeAmigo == '') {
+    alert("Por favor insira um nome!");
+    return
+  } 
+  console.log(nomeAmigo);
 }
