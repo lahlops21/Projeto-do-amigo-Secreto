ec904b2df8bcdee348af127bea7c662a77dc1d41
Lais Lopes <laisllopess21@gmail.com>
2025-03-09 15:22:00 -0300

    Sorteando nomes do array

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -19,6 +19,33 @@ function adicionarAmigo() {
   }
   // Tarefa 1.4 - Limpando o campo de entrada
   document.getElementById("amigo").value = "";
+  atualizarLista();
+}
 
-  console.log(listaAmigos);
+function atualizarLista() {
+  let nomesDaLista = document.getElementById("listaAmigos");
+  nomesDaLista.innerHTML = "";
+
+  // 3 - Percorre o array listaAmigos
+  for (let i = 0; i < listaAmigos.length; i++) {
+    let item = document.createElement("li"); // Cria um elemento <li>
+    item.textContent = listaAmigos[i]; // Define o texto como o nome do amigo
+    nomesDaLista.appendChild(item); // Adiciona o <li> na <ul>
+
+    console.log(listaAmigos);
+    console.log(nomesDaLista);
+  }
+}
+
+function sortearAmigo() {
+  if (listaAmigos.length == 0) {
+    alert("Nenhum nome adicionado ainda! Adicione um nome e tente novamente");
+    return;
+  }
+
+  let itemSorteado = Math.floor(Math.random() * listaAmigos.length);
+  console.log("Índice Sorteado:", itemSorteado);
+
+  let amigoSorteado = listaAmigos[itemSorteado];
+  console.log(amigoSorteado);
 }
